5fb1c3da3564a649f34871bc983438d2c09c36ac
Muhammed Gungor <muhammed@hamilinden.com>
2020-09-25 02:21:19 +0300

    Permutations of a string with a specific pattern

---
+++ Loodos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Main Text:");
+            var mainText = Console.ReadLine();
+
+            Console.WriteLine("Input Searchable Text (Pattern for search): ");
+            var patternText = Console.ReadLine();
+            StringPermutation.Compare(mainText, patternText);
+        }
+    }
+}
---
+++ Loodos/StringPermutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loodos
+{
+    public class StringPermutation
+    {
+        public static bool Compare(string mainText, string pattern)
+        {
+            var mainArray = mainText.ToCharArray();
+            var patArray = pattern.ToCharArray();
+
+            Array.Sort(mainArray);
+            Array.Sort(patArray);
+
+            var m = 0;
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                if (patArray[m] == mainArray[i])
+                {
+                    m++;
+                    if (m == patArray.Length)
+                    {
+                        Console.WriteLine($"It's OK! -------- MainText: \"{mainText}\"  , Pattern: \"{pattern}\" => {true}");
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Failed -------- MainText: \"{mainText}\"  , Pattern: \"{pattern}\" => {false}");
+            return false;
+        }
+    }
+}
